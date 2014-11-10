696bfeb1aa5c43a0d461557c90c830f0743e462e
Peter Lorre <me.use.android@gmail.com>
2014-11-09 18:46:07 -0700

    removing google font function

--- header.php
+++ header.php
@@ -29,6 +29,9 @@
 		<!-- <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png"> -->
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		
+		<!--Google Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
 
 		<?php // wordpress head functions ?>
 		<?php wp_head(); ?>

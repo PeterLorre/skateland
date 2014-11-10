a9289a627f8c6f2523e56318143a746e6052ae4d
Peter Lorre <me.use.android@gmail.com>
2014-11-09 23:52:50 -0700

    Adding front page

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+
+								<header>
+
+									<h1 class="page-title" itemprop="headline">#1 Source for Fun!</h1>
+
+								</header>
+
+								<section class=" cf">
+									
+								</section> 
+
+								<footer class="article-footer cf">
+
+								</footer>
+
+						</div>
+
+						<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
